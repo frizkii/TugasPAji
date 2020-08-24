7dc17e97dfbbae3ca23a64c6b8fa0dee09560ea7
hafizxd <hafizamin8116@gmail.com>
2020-08-24 13:21:17 +0700

    testing

--- app/src/main/java/com/example/frizki12rpl012018/MainActivity.java
+++ app/src/main/java/com/example/frizki12rpl012018/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.frizki12rpl012018;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
